b0ad2053342f001ab0a1d8fa2bd366ed17584c5f
Nikechukwu Okoronkwo <nikechukwu@gmail.com>
2026-02-23 20:31:35 -0600

    WIP: Added Meta Wearables SDK integration for Demo View

--- MetaGlassDemo/ContentView.swift
+++ MetaGlassDemo/ContentView.swift
@@ -6,17 +6,74 @@
 //
 
 import SwiftUI
+import MWDATCamera
+import MWDATCore
 
 struct ContentView: View {
+    @State private var wearableInitialized: Bool = false
+//    @State private var wearable:
+    @State private var wearableInitializationError: Error? = nil
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if wearableInitialized {
+                
+            } else if let error = wearableInitializationError {
+                Text(verbatim: "Oh no, an error occured during initialization: \(String(describing: error))")
+            } else {
+                Text("Awaiting wearable initialization...")
+            }
         }
         .padding()
+        .task {
+            do {
+                let success = try? await initializeWearableDevice()
+                if let s = success {
+                    DispatchQueue.main.async {
+                        self.wearableInitialized = s
+                    }
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    wearableInitializationError = error
+                }
+            }
+        }
     }
+    
+    func initializeWearableDevice() async throws -> Bool {
+        try configureWearables() // configure wearable
+        try await startRegistration() // register device
+    }
+    
+    func deinitWearableDevice() async throws -> Bool {
+        try await startUnregistration()
+    }
+    
+    private func configureWearables() throws(WearableError) {
+      do {
+        try Wearables.configure()
+      } catch {
+          throw .configurationFailed(error)
+      }
+    }
+    
+    
+    func startRegistration() async throws {
+      try await Wearables.shared.startRegistration()
+    }
+
+    func startUnregistration() async throws {
+      try await Wearables.shared.startUnregistration()
+    }
+
+    func handleWearablesCallback(url: URL) async throws {
+      _ = try await Wearables.shared.handleUrl(url)
+    }
+}
+
+enum WearableError: Error {
+    case configurationFailed(_ error: Error?)
 }
 
 #Preview {
